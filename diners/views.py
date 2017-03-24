a7b7e08532c452e7fdb87780acdb8cb0bb627c2c
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-03-24 12:07:16 -0600

    checking errors

--- diners/views.py
+++ diners/views.py
@@ -6,9 +6,39 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from diners.models import AccessLog
 from django.views.decorators.csrf import csrf_exempt
+from django.core.paginator import Paginator
 
 from .models import AccessLog, Diner
 from cloudkitchen.settings.base import PAGE_TITLE
+
+def diners_paginator(request, queryset, num_pages):
+    result_list = Paginator(queryset, num_pages)
+
+    try:
+        num_page = int(request.GET['num_page'])
+    except:
+        num_page = 1
+
+    if num_page <= 0:
+        num_page = 1
+
+    if num_page > result_list.num_pages:
+        num_page = result_list.num_pages
+    
+    if result_list.num_pages >= num_page:
+        page = result_list.page(num_page)
+    
+        context = {
+            'queryset': page.object_list,
+            'num_page': num_page,
+            'pages': result_list.num_pages,
+            'has_next': page.has_next(),
+            'has_prev': page.has_previous(),
+            'next_page': num_page + 1,
+            'prev_page': num_page - 1,
+            'first_page': 1,
+        }
+    return context
 
 @csrf_exempt
 def RFID(request):
@@ -36,14 +66,16 @@ def RFID(request):
 
 
 def diners(request):
-    objects = AccessLog.objects.all().prefetch_related('diner')
-
+    diners_access_log = AccessLog.objects.all().order_by('-access_to_room')
+    pag = diners_paginator(request, diners_access_log, 50)
     template = 'diners.html'
     title = 'Comensales del Dia'
     page_title = PAGE_TITLE
+
     context={
         'title': PAGE_TITLE + ' | ' + title,
         'page_title': title,
-        'objects' : objects
+        'diners' : pag['queryset'],
+        'paginator': pag,
     }
     return render(request, template, context)    
