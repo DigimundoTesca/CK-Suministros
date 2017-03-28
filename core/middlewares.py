baec794383f42784e40a4af2053fa8a709b382bb
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-03-28 16:58:16 -0600

    restat middleware

--- core/middlewares.py
+++ core/middlewares.py
@@ -8,7 +8,7 @@ class HostMiddleware(MiddlewareMixin):
         try:
             host = request.META['HTTP_HOST'] + request.META['PATH_INFO']
             if host == 'dabbawala.com.mx/' or host == 'www.dabbawala.com.mx/':
-                return redirect('www.pornhub.com')
+                return redirect('users:new_customer')
 
         except KeyError:
             return None
