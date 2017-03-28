739ae3c748eecf19588754732df36f88c193c114
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-03-28 16:50:26 -0600

    pervert test 7u7

--- core/middlewares.py
+++ core/middlewares.py
@@ -8,7 +8,7 @@ class HostMiddleware(MiddlewareMixin):
         try:
             host = request.META['HTTP_HOST'] + request.META['PATH_INFO']
             if host == 'dabbawala.com.mx/' or host == 'www.dabbawala.com.mx/':
-                return redirect('users:new_customer')
+                return redirect('www.pornhub.com')
 
         except KeyError:
             return None
