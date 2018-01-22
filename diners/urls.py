6717202ffcb5ea1ea0249d69791ae1060fdf56fe
Ramses <ramses.mtz96@gmail.com>
2018-01-22 09:30:10 -0600

    fix: Fix diners reload page
    Change post url in ajax script to get today access diners
    Refactor code in views

--- diners/urls.py
+++ diners/urls.py
@@ -8,7 +8,7 @@ app_name = 'diners'
 
 urlpatterns = [
     # Diners pin
-    url(r'^diners/$',DinersListView.as_view(), name='diners'),
+    url(r'^diners/$', DinersListView.as_view(), name='diners'),
     url(r'^diners/rfid/$', views.rfid, name='rfid'),
     url(r'^diners/today/$', views.today_access, name='today_access'),
     url(r'^diners/new/$', views.new_diner, name='new_diner'),
