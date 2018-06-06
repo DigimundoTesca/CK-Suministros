1abdef868f035f5c21079239cd2e5a3ea1720b8c
Ramses Martinez <ramses.mtz96@gmail.com>
2018-06-06 05:01:24 -0500

    Fix: Fix searchc query Workers

--- branchoffices/context_processors.py
+++ branchoffices/context_processors.py
@@ -1,10 +1,16 @@
+from users.models import User
 from .models import Worker, BranchOffice
 
 
 def branch_offices_processor(request):
-    user = request.user
-    branch_office = Worker.objects.values('branch_office').get(worker=user.id)
-    if branch_office:
-        branch_office_id = branch_office['branch_office']
-        branch_office = BranchOffice.objects.get(id=branch_office_id)
-        return {'g_branch_office': branch_office}
+    try:
+        user = request.user
+        branch_office = Worker.objects.values('branch_office').get(worker=user.id)
+        if branch_office:
+            branch_office_id = branch_office['branch_office']
+            branch_office = BranchOffice.objects.get(id=branch_office_id)
+            return {'g_branch_office': branch_office}
+    except User.DoesNotExist:
+        user = None
+
+
